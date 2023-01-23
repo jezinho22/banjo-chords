614c1df895649ef225f9342360cdefa1b47c07d4
Jeremy Johns <jez.johns@gmail.com>
2023-01-23 22:44:52 +0000

    need to organise chord data

---
+++ chords.js
@@ -0,0 +1,8 @@
+const songs = {
+  "Angel Monty": {
+    name: "Angel Monty",
+    chords: "GC  GC &#10;GC  DG&#10;G F C G x3&#10;G C D G&#10;",
+    capo: "",
+  },
+};
+console.log(songs["Angel Monty"].chords);
